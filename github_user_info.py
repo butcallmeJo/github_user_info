cbfe8a5665386719311d9951d243c5029c209c2a
butcallmejo <josquin.gaillard@gmail.com>
2016-10-02 12:05:44 -0700

    starting the addition of limiting number of repos to look thru

--- github_user_info.py
+++ github_user_info.py
@@ -40,6 +40,22 @@ def print_user_info(user_resp):
     print "\tupdated:\t" + str(
         convert_github_datetime(user_json.get("updated_at"))
         )
+    # print a newline to separate from rest of program
+    print
+
+def print_repo_activity(repo):
+    """function to print relevant information about the repository
+    repo: json object - individual repository information to get
+    information from.
+    """
+    print "\t\tfork:\t\t" + str(repo.get("fork"))
+    print "\t\tmain language:\t" + str(repo.get("language"))
+    print "\t\tcreated:\t" + str(
+        convert_github_datetime(repo.get("created_at"))
+        )
+    print "\t\tupdated:\t" + str(
+        convert_github_datetime(repo.get("updated_at"))
+        )
 
 def print_user_repos_info(repos_resp):
     """function to print basic repository information
@@ -48,14 +64,18 @@ def print_user_repos_info(repos_resp):
     """
 
     repos_json = repos_resp.json()
+    print "Repositories:"
+    for repo in repos_json:
+        print "\t" + str(repo.get("name"))
+        print_repo_activity(repo) # TODO
     # print repos_json
 
-def get_github_user_api(username, user_info):
+def get_github_user_api(username, user_info, number_repos):
     """function to get the correct path to the github user's API
     username: string - the Github username to get the information from the API
     user_info: boolean - tells the program wether or not to print user info
     """
-    
+
     if user_info:
         user_resp = requests.get(
             'https://api.github.com/users/' + username, auth=gh_config.auth
@@ -64,6 +84,7 @@ def get_github_user_api(username, user_info):
             print_user_info(user_resp)
         else:
             print "Error getting user info." + str(user_resp)
+    # if number_repos:
     repos_resp = requests.get(
         'https://api.github.com/users/' + username + '/repos',
         auth=gh_config.auth
@@ -86,14 +107,20 @@ def main(argv):
     )
     parser.add_argument(
         '--user_info', dest='user_info', action='store_true',
+        default=False,
         help=("More info on the user")
     )
-    parser.set_defaults(user_info=False)
+    parser.add_argument(
+        '--number_repos', dest='number_repos', metavar='N', type=int,
+        default=None,
+        help=("The N most committed repos.")
+    )
     args = parser.parse_args(argv[1:])
     user = args.username
     user_info = args.user_info
+    number_repos = args.number_repos
 
-    get_github_user_api(user, user_info)
+    get_github_user_api(user, user_info, number_repos)
 
 if __name__ == "__main__":
     main(sys.argv)
